97acdd1cb67f3838caca3df1b1211ac138a8a703
DESKTOP-1Q2BB2F\SSJ <sh54991210@gmail.com>
2024-08-12 20:23:43 +0900

    20:20 / Recruit 게시글 목록, 상세보기, 수정, 삭제 기능만 구현 완료

--- Running/src/main/java/com/example/running/recruit/controller/RecruitController.java
+++ Running/src/main/java/com/example/running/recruit/controller/RecruitController.java
@@ -1,5 +1,6 @@
 package com.example.running.recruit.controller;
 
+import com.example.running.recruit.domain.Recruit;
 import com.example.running.recruit.dto.RecruitDTO;
 import com.example.running.recruit.service.RecruitService;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,6 +29,37 @@ public class RecruitController {
     @GetMapping("/list")
     public ResponseEntity<Object> getAllRecruits() {
         return new ResponseEntity<>(recruitService.findAllRecruits(), HttpStatus.OK);
+    }
+
+    @GetMapping("/read")
+    public ResponseEntity<Object> getRecruitById(@RequestParam Long rno) {
+        Optional<Recruit> oneRecruit = recruitService.findOneRecruit(rno);
+
+        if(oneRecruit.isPresent()) {
+            return ResponseEntity.ok(oneRecruit.get());
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("post not found ------- no : " + rno);
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<Object> updateRecruit(@RequestBody RecruitDTO recruitDTO) {
+        try {
+            Recruit modifyRecruit = recruitService.modifyRecruit(recruitDTO);
+            return new ResponseEntity<>(modifyRecruit, HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            log.error("post not found ------- no : " + recruitDTO.getRno(), e);
+            return new ResponseEntity<>("post not found ------- no : " + recruitDTO.getRno(), HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            log.error("Error modifying recruit post", e);
+            return new ResponseEntity<>("Error modifying recruit post", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("{rno}")
+    public ResponseEntity<Object> deleteRecruit(@PathVariable Long rno) {
+        recruitService.deleteRecruit(rno);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
