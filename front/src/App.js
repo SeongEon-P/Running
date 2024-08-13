f8a96bc9869376a912e266b982aba03fc64c1070
박성언 <tjddjs909@naver.com>
2024-08-13 11:28:52 +0900

    메인페이지에 이미지 안불러와지는 문제 해결

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import './App.css';
+import MainPage from "./main/MainPage";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React_PSE test
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+       <Route path="/" element={<MainPage />} />
+      </Routes>
+    </Router>
   );
 }
 
