1c427e3c6f442aa24dac2da177f269cda05b67b8
SangJun Seo <nice92x@naver.com>
2024-08-16 21:51:04 +0900

    병합후 기타 수정

--- Running/src/main/java/com/example/running/member/domain/Member.java
+++ Running/src/main/java/com/example/running/member/domain/Member.java
@@ -19,8 +19,6 @@ public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     @Column(nullable = false, unique = true, length = 20)
     private String mid;
 
