ea84b16b4bc008aa62827159052ed20dc7ae35e1
DESKTOP-1Q2BB2F\SSJ <sh54991210@gmail.com>
2024-08-20 21:33:58 +0900

    21:30 / 지도 위에 컨트롤바 표출, 인포윈도우 설정(기본 마커 위에 표시, 닫을 수 있음, 클릭하면 다시 나타남, 길찾기 링크 누르면 카카오 맵으로 이동 됨)

--- front/src/recruit/RecruitRead.jsx
+++ front/src/recruit/RecruitRead.jsx
@@ -164,11 +164,47 @@ const RecruitRead = () => {
                 };
                 const map = new kakao.maps.Map(container, options); // 지도 생성 및 객체 리턴
 
+                // 지도 위에 마커 표시
                 const markerPosition = options.center;
                 const marker = new kakao.maps.Marker({
                     position: markerPosition
                 });
                 marker.setMap(map);
+
+                // 컨트롤바 표시 
+                const mapTypeControl = new kakao.maps.MapTypeControl();
+
+                // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+                // kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+                map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+                // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+                const zoomControl = new kakao.maps.ZoomControl();
+                map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+                // 인포윈도우 설정
+                // 인포윈도우 내용
+                const iwContent = `<div style="padding:5px;">${recruit.r_place2} <br><a href="https://map.kakao.com/link/to/${recruit.r_place2},${y},${x}" style="color:blue" target="_blank">길찾기</a></div>`;
+                // 인포윈도우의 위치
+                const iwPosition = options.center;
+                // iwRemoveable을 true로 설정하면 인포윈도우를 닫을 수 있는 x 표시 생성
+                const iwRemoveable = true;
+
+                // 인포윈도우를 생성합니다
+                const infowindow = new kakao.maps.InfoWindow({
+                    position: iwPosition,
+                    content: iwContent,
+                    removable : iwRemoveable
+                });
+
+                // 마커 위에 인포윈도우를 표시합니다. 두번째 파라미터인 marker를 넣어주지 않으면 지도 위에 표시됩니다
+                infowindow.open(map, marker);
+
+                // 마커에 클릭이벤트를 등록합니다
+                kakao.maps.event.addListener(marker, 'click', function () {
+                    // 마커 위에 인포윈도우를 표시합니다
+                    infowindow.open(map, marker);
+                });
             }
 
 
@@ -234,8 +270,8 @@ const RecruitRead = () => {
                         </tr>
                     )}
                 </tbody>
-                {location.x && location.y && 
-                <KakaoMap x={location.x} y={location.y} />}
+                {location.x && location.y &&
+                    <KakaoMap x={location.x} y={location.y} />}
             </div>
         </>
     )
