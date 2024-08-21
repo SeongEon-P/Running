3829c938ec25fbe96c3053443eaf7b0acaca54ef
박성언 <tjddjs909@naver.com>
2024-08-21 14:04:56 +0900

    기본 사이드바 생성_병합 전 푸시

--- front/src/pages/FreeBoard/FreeBoardList.js
+++ front/src/pages/FreeBoard/FreeBoardList.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Sidebar from '../../components/Sidebar/Sidebar';  // Sidebar 추가
 import './FreeBoard.css';
 
 const FreeBoardList = ({ posts }) => {
     return (
-        <div className="free-board">
-            <h1>Free Board</h1>
-            <Link to="/free/create">
-                <button>글작성</button>
-            </Link>
-            <div className="posts">
-                {posts.map((post, index) => (
-                    <div key={index} className="post">
-                        <h2>{post.title}</h2>
-                        <p>{post.content}</p>
-                        <button>수정</button>
-                        <button>삭제</button>
-                    </div>
-                ))}
+        <div className="free-board-container">
+            <Sidebar /> {/* Sidebar 컴포넌트 추가 */}
+            <div className="free-board">
+                <h1>Free Board</h1>
+                <Link to="/free/create">
+                    <button>글작성</button>
+                </Link>
+                <div className="posts">
+                    {posts.map((post, index) => (
+                        <div key={index} className="post">
+                            <h2>{post.title}</h2>
+                            <p>{post.content}</p>
+                            <button>수정</button>
+                            <button>삭제</button>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
