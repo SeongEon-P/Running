6ef6960bd934c53ece7eac3a1279a3be5100a3eb
SangJun Seo <nice92x@naver.com>
2024-09-02 21:51:07 +0900

    test

--- Running/src/main/java/com/example/running/recruit/repository/RecruitRepository.java
+++ Running/src/main/java/com/example/running/recruit/repository/RecruitRepository.java
@@ -10,16 +10,29 @@ import java.time.LocalTime;
 import java.util.List;
 
 public interface RecruitRepository extends JpaRepository<Recruit, Long> {
-    @Query("SELECT r FROM Recruit r WHERE " +
-            "(LOWER(r.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
-            "LOWER(r.content) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
-            "LOWER(r.memberRecruit.mid) LIKE LOWER(CONCAT('%', :keyword, '%'))) and " +
-            "(r.date between :startDate and :endDate) and " +
-            "(r.time between :startTime and :endTime) ")
-    List<Recruit> searchRecruitsByKeywordAndDateTime(
-            @Param("keyword") String keyword,
-            @Param("startDate") LocalDate startDate,
-            @Param("endDate") LocalDate endDate,
-            @Param("startTime") LocalTime startTime,
-            @Param("endTime") LocalTime endTime);
+//    @Query("SELECT r FROM Recruit r WHERE " +
+//            "(LOWER(r.title) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+//            "LOWER(r.content) LIKE LOWER(CONCAT('%', :keyword, '%')) OR " +
+//            "LOWER(r.memberRecruit.mid) LIKE LOWER(CONCAT('%', :keyword, '%'))) and " +
+//            "(r.date between :startDate and :endDate) and " +
+//            "(r.time between :startTime and :endTime) ")
+//    List<Recruit> searchRecruitsByKeywordAndDateTime(
+//            @Param("keyword") String keyword,
+//            @Param("startDate") LocalDate startDate,
+//            @Param("endDate") LocalDate endDate,
+//            @Param("startTime") LocalTime startTime,
+//            @Param("endTime") LocalTime endTime);
+@Query("SELECT r FROM Recruit r WHERE " +
+        "(LOWER(r.title) LIKE :keywordPattern OR " +
+        "LOWER(r.content) LIKE :keywordPattern OR " +
+        "LOWER(r.memberRecruit.mid) LIKE :keywordPattern) and " +
+        "(r.date between :startDate and :endDate) and " +
+        "(r.time between :startTime and :endTime) ")
+List<Recruit> searchRecruitsByKeywordAndDateTime(
+        @Param("keywordPattern") String keywordPattern,
+        @Param("startDate") LocalDate startDate,
+        @Param("endDate") LocalDate endDate,
+        @Param("startTime") LocalTime startTime,
+        @Param("endTime") LocalTime endTime);
+
 }
